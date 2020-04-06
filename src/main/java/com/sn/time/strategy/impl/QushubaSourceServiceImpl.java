b51aba16038aeaed018867ea387da29922763022
songning <1457065857@qq.com>
2020-04-06 14:40:10 +0800

    趣书吧

--- src/main/java/com/sn/time/strategy/impl/QushubaSourceServiceImpl.java
+++ src/main/java/com/sn/time/strategy/impl/QushubaSourceServiceImpl.java
@@ -1,8 +1,18 @@
 package com.sn.time.strategy.impl;
 
+import com.sn.time.entity.Chapters;
+import com.sn.time.service.ChaptersService;
+import com.sn.time.service.impl.ChaptersServiceImpl;
 import com.sn.time.strategy.SourceService;
-import org.springframework.stereotype.Service;
+import com.sn.time.util.BeanUtil;
+import com.sn.time.util.DateUtil;
+import com.sn.time.util.HttpUtil;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 
+import java.util.Date;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -12,5 +22,41 @@ import java.util.Map;
 public class QushubaSourceServiceImpl implements SourceService {
     @Override
     public void updateChapter(Map<String, Object> map) {
+        String sourceUrl = String.valueOf(map.get("sourceUrl"));
+        String novelsId = String.valueOf(map.get("novelsId"));
+        Document baseDoc = HttpUtil.getHtmlFromUrl(sourceUrl, true);
+        Element maininfoElement = baseDoc.getElementById("maininfo");
+        Element infoElement = maininfoElement.getElementById("info");
+        String latestChapter = infoElement.getElementsByTag("p").get(3).getElementsByTag("a").get(0).html();
+        ChaptersService chaptersService = BeanUtil.getBean(ChaptersServiceImpl.class);
+        List<Map<String, Object>> isExistList = chaptersService.findByNovelsId(novelsId);
+        String lastChapter = String.valueOf(isExistList.get(isExistList.size() - 1).get("chapter"));
+        if (!latestChapter.equals(lastChapter)) {
+            Element dlElement = baseDoc.getElementById("list").getElementsByTag("dl").get(0);
+            Elements ddList = dlElement.getElementsByTag("dd");
+            Date oldUpdateTime = (Date) isExistList.get(isExistList.size() - 1).get("updateTime");
+            String[] times = (infoElement.getElementsByTag("p").get(2).html().split("：")[1]).split(" ");
+            String strUpdateTime = times[0] + " " + times[1] + ":00";
+            Date newUpdateTime = DateUtil.strToDate(strUpdateTime, "yyyy-MM-dd HH:mm:ss");
+            List<Date> timeList = DateUtil.stepTime(oldUpdateTime, newUpdateTime, ddList.size() - isExistList.size());
+            for (int i = isExistList.size(), iLen = ddList.size(); i < iLen; i++) {
+                Element a = ddList.get(i).getElementsByTag("a").get(0);
+                String chapter = a.html();
+                boolean isExist = false;
+                for (Map<String, Object> item : isExistList) {
+                    if (chapter.equals(String.valueOf(item.get("chapter")))) {
+                        isExist = true;
+                        break;
+                    }
+                }
+                if (!isExist) {
+                    String chapterUrl = sourceUrl + a.attr("href");
+                    Document contentDoc = HttpUtil.getHtmlFromUrl(chapterUrl, true);
+                    String content = contentDoc.getElementById("content").html();
+                    Chapters chapters = Chapters.builder().chapter(chapter).content(content).novelsId(novelsId).updateTime(timeList.get(i - isExistList.size())).build();
+                    chaptersService.save(chapters);
+                }
+            }
+        }
     }
 }
