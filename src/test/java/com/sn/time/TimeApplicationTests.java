37708f3ab0dce70d689cc22e5335319be6cac4b1
songning <1457065857@qq.com>
2020-04-11 16:21:30 +0800

    删除多余代码

--- src/test/java/com/sn/time/TimeApplicationTests.java
+++ src/test/java/com/sn/time/TimeApplicationTests.java
@@ -10,7 +10,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import javax.annotation.Resource;
-import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.Executor;
@@ -20,8 +19,6 @@ import java.util.concurrent.Executor;
 @SpringBootTest
 class TimeApplicationTests {
 
-    @Autowired
-    private TimeProcessor timeProcessor;
     @Autowired
     private NovelsRepository novelsRepository;
     @Autowired
@@ -34,19 +31,5 @@ class TimeApplicationTests {
         String novelsId = "402880ea714a6c0401714a71de6f000d";
         List<Map<String, Object>> result = chaptersRepository.findByNovelsIdNative(novelsId);
         System.out.println("");
-    }
-
-    @Test
-    public void testBiquge() {
-        List<String> sourceList = Arrays.asList("笔趣阁", "147小说", "天天书吧", "飞库小说", "趣书吧");
-        for (String sourceName : sourceList) {
-            sourceExecutor.execute(() -> {
-                List<Map<String, Object>> novelsList = novelsRepository.findBySourceNameNative(sourceName);
-                // 排除最后一个正在新增的小说
-                for (int i = 0, length = novelsList.size(); i < length - 1; i++) {
-                    timeProcessor.timeExecutor(novelsList.get(i));
-                }
-            });
-        }
     }
 }
